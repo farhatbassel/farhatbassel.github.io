fd666287502b20076ea786d272031c53d472e186
farhatbassel <73551211+farhatbassel@users.noreply.github.com>
2021-08-06 03:38:21 +0200

    email issue

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,5 @@
 import './contact.scss'
 import { useState } from 'react';
-import 'index.php'
 
 export default function Contact() {
 
@@ -19,11 +18,11 @@ export default function Contact() {
             </div>
             <div className="wrapper">
             <div className='left'>
-                <form onSubmit={handleSubmit} method='post' action= 'index.php'>
+                <form onSubmit={handleSubmit} method='post' action= 'https://formsubmit.co/farhatbassel@tutanota.com'>
                     <h2>To contact me, submit the form below</h2>
-                    <input type='text' name='name' placeholder='Name'/>
-                    <input type='text' name='name' placeholder='Email'/>
-                    <textarea placeholder='Message' name='message'></textarea>
+                    <input type='text' name='name' placeholder='Name' required/>
+                    <input type='text' name='name' placeholder='Email' required/>
+                    <textarea placeholder='Message' name='message' required></textarea>
                     <button type='submit' name='submit' value="Send Message">Submit</button>
                     {message && <span>Thank you, I'll get back to you as soon as possible :)</span>}
                 </form>
